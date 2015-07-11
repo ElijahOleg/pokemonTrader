f8bc9ab48c25f50035647e2de3543adf1a3e3897
bonnieso <so_bonnie@hotmail.com>
2015-07-11 13:09:23 -0700

    adding additional node packages for passport

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.require('./config/passport')(passport);
+require('./config/passport')(passport);
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
